8c7de5f8178e1b90479fcd22ba4f3c31cd765a7a
Ali Shobeiri <alishobeiri@ymail.com>
2019-01-26 14:17:49 -0500

    Fixed server host

--- Backend/server.py
+++ Backend/server.py
@@ -4,18 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Index!"
- 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
- 
-@app.route("/members")
-def members():
-    return "Members"
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return "Test"
+
  
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
