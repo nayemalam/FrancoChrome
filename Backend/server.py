d4f920b7063d666938d9662619bf3771776be7d1
Ali Shobeiri <alishobeiri@ymail.com>
2019-01-26 16:10:06 -0500

    Added image route

--- Backend/server.py
+++ Backend/server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from datetime import date, datetime
+from flask import Flask, request, send_file
+import io
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,6 +10,19 @@ CORS(app, resources={"/*": {"origins": "*"}})
 def index():
     return "Index!"
 
+
+@app.route("/image/")
+def image():
+    date_arg = request.args.get('date')
+    if date_arg is None:
+        date_arg = date.today()
+    else:
+        date_arg = datetime.strptime(date_arg, "%m/%d/%Y")
+    
+    print(date_arg)
+    
+    return send_file("./images/quebec_small.jpg", mimetype="image/jpg")
+
  
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
