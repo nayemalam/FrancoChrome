e29bf1d08ed4d95b9429a99bf3e5b9bf1745b596
Afreen Aliya <afreenjuly@gmail.com>
2019-01-26 20:36:51 -0500

    added functions

--- Frontend/chrome_extension/background.js
+++ Frontend/chrome_extension/background.js
@@ -1,46 +1,38 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
+var todayDate = (getAllDate());
+getAllElements(todayDate);
 
-console.log("hello ali bsdfj");
+function getAllDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-
-if (dd < 10) {
-  dd = '0' + dd;
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  today = mm + '/' + dd + '/' + yyyy;
+  return today;
 }
 
-if (mm < 10) {
-  mm = '0' + mm;
+function getAllElements(date){
+  var objectRequest = new XMLHttpRequest();
+  debugger;
+  objectRequest.open("GET", "http://35.203.43.62:8080/image?date="+ date, false);
+  objectRequest.send();
+  document.getElementById('bg').src = "http://35.203.43.62:8080" + objectRequest.responseText;
+  document.getElementById('photo_desc').innerHTML = objectRequest.getResponseHeader("img_description");
+  document.getElementById('word').innerHTML  = objectRequest.getResponseHeader("word");
+  document.getElementById('meaning').innerHTML = objectRequest.getResponseHeader("translation");
+  var audioFile = objectRequest.getResponseHeader("audio_file");
+  playAudio(audioFile);
 }
 
-today = mm + '/' + dd + '/' + yyyy;
-
-console.log("Date:" + today);
-
-
-var imageRequest = new XMLHttpRequest();
-debugger;
-imageRequest.open("GET", "http://35.203.43.62:8080/image?date="+ today, false);
-imageRequest.send();
-document.getElementById('bg').src = "http://35.203.43.62:8080" + imageRequest.responseText;
-document.getElementById('photo_desc').innerHTML = imageRequest.getResponseHeader("img_description");
-document.getElementById('word').innerHTML  = imageRequest.getResponseHeader("word");
-document.getElementById('meaning').innerHTML = imageRequest.getResponseHeader("translation");
-//var newWord = wordRequest.responseText;
-
-/*
-var wordRequest = new XMLHttpRequest();
-wordRequest.open("GET", "http://35.203.43.62:8080/word?date=" + today, true);
-wordRequest.send();
-document.getElementById('word').innerHTML  = wordRequest.responseText;
-document.getElementById('meaning').innerHTML = wordRequest.getResponseHeader("Translation");
-//var newWord = wordRequest.responseText;
-console.log(newWord);
-*/
+function playAudio(audioFile) {
+  document.getElementById('audio').src = audioFile;
+  document.getElementById('audio').play();
+}
