cd7f446f1936ceaad9e66becfa60a49db6cc8a8f
Ali Shobeiri <alishobeiri@ymail.com>
2019-01-27 03:19:25 -0500

    Trash

--- Backend/server.py
+++ Backend/server.py
@@ -7,7 +7,7 @@ import json
 from database import create_connection, create_table
 
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "*"}}, expose_headers='*')
+CORS(app, resources={"/*": {"origins": "*"}})
 
 users = dict()
 images = dict()
